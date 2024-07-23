1dd1754bac1916e3b1d222b6310ee8f7964b576a
Juan Baez <baez.juan@principal.com>
2024-07-22 18:25:13 -0600

    ajustes para seguridad en peticiones

---
+++ Server.Objects/Principal/Server/Objects/Identidad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Principal.Server.Objects
+{
+    [Serializable]
+    public class Identidad
+    {
+        public List<Reclamacion> Reclamaciones { get; set; }
+
+        public byte[] Signature { get; set; }
+    }
+}
